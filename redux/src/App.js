d387bac0088a6cc836f98d8a206e995e46b98100
so0112 <tkddh0112@gmail.com>
2022-07-06 10:55:35 +0900

    리덕스 적용 전

--- redux/src/App.js
+++ redux/src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./style.css";
 
-function App() {
+export default function App() {
+  const [number, setNumber] = useState(1);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="container">
+      <h1>Root : {number}</h1>
+      <div id="grid">
+        <Left1 number={number}></Left1>
+        <Right1
+          onIncrease={() => {
+            setNumber(number + 1);
+          }}
+        ></Right1>
+      </div>
     </div>
   );
 }
 
-export default App;
+function Left1(props) {
+  return (
+    <div>
+      <h1>Left1 : {props.number}</h1>
+      <Left2 number={props.number}></Left2>
+    </div>
+  );
+}
+
+function Left2(props) {
+  return (
+    <div>
+      <h1>Left2 : {props.number}</h1>
+      <Left3 number={props.number}></Left3>
+    </div>
+  );
+}
+
+function Left3(props) {
+  return (
+    <div>
+      <h1>Left3 : {props.number}</h1>
+    </div>
+  );
+}
+
+function Right1(props) {
+  return (
+    <div>
+      <h1>Right1</h1>
+      <Right2
+        onIncrease={() => {
+          props.onIncrease();
+        }}
+      ></Right2>
+    </div>
+  );
+}
+
+function Right2(props) {
+  return (
+    <div>
+      <h1>Right2</h1>
+      <Right3
+        onIncrease={() => {
+          props.onIncrease();
+        }}
+      ></Right3>
+    </div>
+  );
+}
+
+function Right3(props) {
+  return (
+    <div>
+      <h1>Right3</h1>
+      <input
+        type="button"
+        value="+"
+        onClick={() => {
+          props.onIncrease();
+        }}
+      ></input>
+    </div>
+  );
+}
+
